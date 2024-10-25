db8895a97aeb4720f2711c45c9abc5fe87ea7d22
Md Jahid Hasan Anik <jahid.prolific@gmail.com>
2024-10-25 18:01:53 +0600

    TMDB Api key generated

--- utility/constants.js
+++ utility/constants.js
@@ -1,4 +1,4 @@
-export const apiBaseUrl = 'https://gutendex.com';
+export const apiBaseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=0d93f1002d7d96fd63609b93bb5a53c9';
 export const Constants = {
     Api: {
         books: {
